5d2591050647cb31fdab90897544887ca6318fe3
Aisha Al-Semaiti <41858301+aisha-als@users.noreply.github.com>
2024-03-14 15:19:24 +0000

    Adding requirements.txt and setup.py files.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='client-server-network',
+    version='1.0',
+    description='This is a Python package to use the Python socket module to create a client / server network.',
+    packages=['clientserver'],
+    install_requires=[
+    ],
+)
