7c5a4721ab4f5c367bbb7b9fd0584ccac6410d09
sgaberth <158296334+sgaberth@users.noreply.github.com>
2024-03-14 17:46:13 +0000

    Add files via upload

---
+++ print to screen and file.py
@@ -0,0 +1,29 @@
+import sys
+
+# This program redirects the print() to the file by routing the standard output sys.stdout
+# to point to the destination file instead.
+# It saves the current stdout so that we can revert sys.stdou after we complete
+# our redirection
+
+stdout_fileno = sys.stdout
+
+sample_input = ['Hi', 'all', 'exit']
+
+# Redirect sys.stdout to the file
+sys.stdout = open('output.txt', 'w')
+
+for ip in sample_input:
+    # Prints to the redirected stdout (Output.txt)
+    sys.stdout.write(ip + '\n')
+    # Prints to the actual saved stdout handler
+    stdout_fileno.write(ip + '\n')
+
+
+# Close the file
+sys.stdout.close()
+# Restore sys.stdout to our old saved file handler
+sys.stdout = stdout_fileno
+
+
+my_file = open("output.txt", "r")
+print(my_file.read())
