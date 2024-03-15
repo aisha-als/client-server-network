dea0dcbcf6bc2dc0733263640e6586e4aaac4836
sgaberth <158296334+sgaberth@users.noreply.github.com>
2024-03-15 17:36:04 +0000

    Update server.py

--- clientserver/server.py
+++ clientserver/server.py
@@ -1,37 +1,67 @@
 import socket
+import sys
+import decrypt
+import file_printer
 
 # Define host and port - local host uses local IP of computer
 HOST = 'localhost'
 PORT = 6666
 
-# Creates a socket object - AF_INET specifies IPv4 - SOCK_STREAM specifies TCP socket type
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
-    # Binds server_socket to the host and port defined above
-    server_socket.bind((HOST, PORT))
+def receive_data(print_to_file=False, decryption=False):
 
-    # Starts listening for incoming connections
-    server_socket.listen()
+    # Creates a socket object - AF_INET specifies IPv4 - SOCK_STREAM specifies TCP socket type
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
-    print(f"Server is listening for connections to {HOST}:{PORT}")
+        # Binds server_socket to the host and port defined above
+        server_socket.bind((HOST, PORT))
 
-    # Accepts incoming connections
-    connection, address = server_socket.accept()
+        # Starts listening for incoming connections
+        server_socket.listen()
 
-    with connection:
-        print(f"Server connected to {address}")
+        print(f"Server is listening for connections to {HOST}:{PORT}")
 
-        # Receives data from client side - 1024 represents max number of bytes that can be recieved at once
-        while True:
-            data = connection.recv(1024)
+        # Accepts incoming connections
+        connection, address = server_socket.accept()
 
-            # If data recieved is empty, then will break loop
-            if not data:
-                break
+        with connection:
+            print(f"Server connected to {address}")
 
-            # Decodes data by converting bytes to string then printing
-            # (needs adjusting for other file types e.g. JSON, binary etc)
-            print(f"Data recieved: {data.decode()}")
-            
-            # Sends a copy of the data recieved back to client to confirm its receipt
-            connection.sendall(data)
+            # Receives data from client side - 1024 represents max number of bytes that can be recieved at once
+            while True:
+                data = connection.recv(1024)[0]
+                key = connection.recv(1024)[1]
+                if decryption:
+                    decrypt.symmetric_decryption(data, key)
+
+                # If data recieved is empty, then will break loop
+                if not data:
+                    break
+
+                # Decrypt if decryption is required
+                if decryption:
+                    data = decrypt.symmetric_decryption(data, key)
+                # Decodes data by converting bytes to string then printing
+                # (needs adjusting for other file types e.g. JSON, binary etc)
+                print(f"Data recieved: {data.decode()}")
+
+                # Print to file if required
+                if print_to_file:
+                    file_printer.writ(data)
+
+                # Sends a copy of the data recieved back to client to confirm its receipt
+                connection.sendall(data)
+
+
+if __name__ == '__main__':
+
+    # Select if the data is to be print to file
+    # Options: True, False
+    print_to_file = True
+
+    # Select if the data is to be decrypted
+    # Options: True, False
+    decryption = False
+
+    # Call the main script
+    receive_data(print_to_file, decryption)
