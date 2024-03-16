49c484e41ed6827a01f4c5ae88efdf65dd695c2b
sgaberth <158296334+sgaberth@users.noreply.github.com>
2024-03-16 14:24:45 +0000

    Update encryption.py

--- clientserver/encryption.py
+++ clientserver/encryption.py
@@ -1,5 +1,4 @@
-from cryptography.fernet import Fernet
-
+from Crypto.Cipher import AES
 
 def open_file():
     # Open the .txt file
@@ -7,14 +6,11 @@ def open_file():
         txt = f.read()
         return txt
 
-
 def symmetric_encryption(file):
-    # Use Fernet to generate an encryption key
-    key = Fernet.generate_key()
-    fernet = Fernet(key)
-
-    # Encrypt the .txt file using the generated key
-    encrypted_txt = fernet.encrypt(file.encode())
+    # Use AES to generate a symmetric key (same key used to encrypt and decrypt)
+    obj1 = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+    # use the to encrypt the message in client side
+    encrypted_text = obj1.encrypt(file.encode())
 
     # Print statements to view the unencrypted and encrypted versions
     print("Unencrypted file: ", file)
@@ -23,24 +19,16 @@ def symmetric_encryption(file):
     # Return the encrypted text
     return encrypted_txt
 
-
 def symmetric_decryption(file):
-    # Use the same key as the encryption
-    key = Fernet.generate_key()
-    fernet = Fernet(key)
-    # Instance the Fernet class with the key
-    fernet = Fernet(key)
-
-    # decrypt the encrypted string with the
-    # Fernet instance of the key,
-    # that was used for encrypting the string
-    # encoded byte string is returned by decrypt method,
-    # so decode it to string with decode methods
-    decrypted_txt = fernet.decrypt(file).decode()
+    # Generate the same key as the encryption key 
+    # if the same passphrase and the initialization vector(IV) are used
+    # this decryption key can used in server side
+    obj2 = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+    # decryption in sever side
+    decrypted_text = obj2.decrypt(file).decode()
 
     print("decrypted string: ", decrypted_txt)
     return decrypted_txt
-
 
 if __name__ == '__main__':
     txt_file = open_file()
