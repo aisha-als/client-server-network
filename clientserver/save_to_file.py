2395535e15d18ee1c565f00af91ae2ae79e0a29d
Emilia Fenn-Newbury <emilia.fenn@gmail.com>
2024-03-17 19:32:07 +0000

    Addition of exception handling and docstrings.

--- clientserver/save_to_file.py
+++ clientserver/save_to_file.py
@@ -1,9 +1,9 @@
 import sys
 
-# This program redirects the print() to the file by routing the standard output sys.stdout
-# to point to the destination file instead.
-# It saves the current stdout so that we can revert sys.stdou after we complete
-# our redirection
+""" This program redirects the print() to the file by routing the standard output sys.stdout
+to point to the destination file instead.
+It saves the current stdout so that we can revert sys.stdou after we complete
+our redirection. """
 
 sample_input = ['Hi', 'all', 'exit']
 
@@ -25,10 +25,15 @@ def writ(input_data):
 
     # Restore sys.stdout to our old saved file handler
     sys.stdout = stdout_fileno
-
-    my_file = open("output.txt", "r")
-    return my_file
-
+    try:
+        my_file = open("output.txt", "r")
+        return my_file
+    except FileNotFoundError as fnfe:
+        print(f"An error occured in the writ function. The file could not be found: {fnfe}")
+        return 1
+    except Exception as e:
+        print(f"An error occured in the writ function: {e}")
+        return 1
 
 if __name__ == '__main__':
     # Call the main script
